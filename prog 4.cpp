b88532a5a680e7eba1e80904bce1e46ea19a3b9f
Vaishnavi1193 <107098851+Vaishnavi1193@users.noreply.github.com>
2022-08-27 13:43:45 +0530

    Add files via upload

---
+++ prog 4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class employee
+{
+  public:
+  	employee()
+  	{
+  	  cout<<"default constructor invocked"<<endl;
+	}
+};
+int main(void)
+{
+  employee e1;
+  employee e2;
+  return 0;
+}
